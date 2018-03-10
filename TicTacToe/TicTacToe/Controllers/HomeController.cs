ff39774807278af8601ab64743db89bfba87f173
Craig Lawrie <34558661+craiglawrie@users.noreply.github.com>
2018-03-10 18:54:01 -0500

    initial commit

---
+++ TicTacToe/TicTacToe/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TicTacToe.Models;
+
+namespace TicTacToe.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            GameModel game = GetActiveGame();
+            return View(game.Game);
+        }
+
+        [HttpPost]
+        public ActionResult Index(int square)
+        {
+            GameModel game = GetActiveGame(square);
+            PlaySquare(game, square);
+            return RedirectToAction("Index");
+        }
+
+        private void PlaySquare(GameModel game, int square)
+        {
+            if (square == -1)
+            {
+                return;
+            }
+
+            if (game.Game[square] == ' ')
+            {
+                game.Game[square] = 'X';
+                for (int i = 0; i < 9; i++)
+                {
+                    if (game.Game[(square + i) % 9] == ' ')
+                    {
+                        game.Game[(square + i) % 9] = 'O';
+                        return;
+                    }
+                }
+            }
+        }
+
+        private GameModel GetActiveGame(int square = 0)
+        {
+            GameModel game = null;
+
+            if (Session["game"] == null || square == -1)
+            {
+                game = new GameModel();
+                Session["game"] = game;
+            }
+            else
+            {
+                game = Session["game"] as GameModel;
+            }
+
+            return game;
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Models/GameModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicTacToe.Models
+{
+    public class GameModel
+    {
+        public GameModel()
+        {
+            Game = new List<char> { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        }
+
+        public List<char> Game { get; set; }
+    }
+}
